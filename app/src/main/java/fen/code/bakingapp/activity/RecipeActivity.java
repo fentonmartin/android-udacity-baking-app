731699c954c05802477c51a4debf820cc75cbb3c
Fenton Martin <fentonxm@gmail.com>
2017-09-01 16:59:14 +0700

    Add Recipe Activity

---
+++ app/src/main/java/fen/code/bakingapp/activity/RecipeActivity.java
@@ -0,0 +1,67 @@
+package fen.code.bakingapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+
+import fen.code.bakingapp.FenProductions;
+import fen.code.bakingapp.R;
+import fen.code.bakingapp.adapter.RecipeAdapter;
+import fen.code.bakingapp.entity.Recipe;
+import fen.code.bakingapp.test.SimpleIdlingResource;
+import fen.code.bakingapp.util.StringUtils;
+
+public class RecipeActivity extends FenProductions implements
+        RecipeAdapter.ListItemClickListener {
+
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        setHomeDisabled();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
+        getIdlingResource();
+    }
+
+    @Override
+    public void onListItemClick(Recipe selectedItemIndex) {
+        Bundle selectedRecipeBundle = new Bundle();
+        ArrayList<Recipe> selectedRecipe = new ArrayList<>();
+        selectedRecipe.add(selectedItemIndex);
+        selectedRecipeBundle.putParcelableArrayList(StringUtils.SELECTED_RECIPE, selectedRecipe);
+
+        final Intent intent = new Intent(this, RecipeDetailActivity.class);
+        intent.putExtras(selectedRecipeBundle);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
