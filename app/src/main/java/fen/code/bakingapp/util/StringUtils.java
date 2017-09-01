31cd455bceef7178c062d591434f03583ed106b4
Fenton Martin <fentonxm@gmail.com>
2017-09-01 16:22:16 +0700

    Add StringUtils

---
+++ app/src/main/java/fen/code/bakingapp/util/StringUtils.java
@@ -0,0 +1,15 @@
+package fen.code.bakingapp.util;
+
+public class StringUtils {
+
+    public static final String BASE_URL = "http://go.udacity.com/";
+    public static final String BAKING_URL = "android-baking-app-json";
+
+    public static final String RECIPES = "all_recipes";
+    public static final String SELECTED_INDEX = "selected_index";
+    public static final String SELECTED_RECIPE = "selected_recipe";
+    public static final String SELECTED_STEP = "selected_step";
+
+    public static final String STACK_RECIPE_DETAIL = "STACK_RECIPE_DETAIL";
+    public static final String STACK_RECIPE_STEP_DETAIL = "STACK_RECIPE_STEP_DETAIL";
+}
