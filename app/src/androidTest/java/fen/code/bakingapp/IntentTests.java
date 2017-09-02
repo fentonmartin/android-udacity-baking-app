9848265b24a01ac1552f9576a3f6fc9a1182fce3
Fenton Martin <fentonxm@gmail.com>
2017-09-02 10:44:17 +0700

    Add Espresso Testing on Activity and Intent Tests

---
+++ app/src/androidTest/java/fen/code/bakingapp/IntentTests.java
@@ -0,0 +1,67 @@
+package fen.code.bakingapp;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import fen.code.bakingapp.activity.RecipeActivity;
+import fen.code.bakingapp.activity.RecipeDetailActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class IntentTests {
+
+    @Rule
+    public IntentsTestRule<RecipeActivity> mActivityTestRule =
+            new IntentsTestRule<RecipeActivity>(RecipeActivity.class);
+
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        // To prove that the test fails, omit this call:
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+    @Before
+    public void stubAllExternalIntents() {
+        intending(not(isInternal())).respondWith(new
+                Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    @Test
+    public void checkIntent_RecipeDetailActivity() {
+        onView(ViewMatchers.withId(R.id.recipe_recycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        intended(hasComponent(RecipeDetailActivity.class.getName()));
+
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+}
